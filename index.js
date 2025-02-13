b9cc19c2f6aff13700f6eb02f6db748f1423cb4b
Jupiterbold <158526130+Jupiterbold@users.noreply.github.com>
2025-02-13 08:38:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ require("./plugins/" + plugin);
 console.log('Plugins installed successful ✅')
 console.log('Bot connected to whatsapp ✅')
 
-  let up = `Platinum-v2 connected successful ✅\n\nPREFIX: ${prefix}`;
+  let up = `Platinum-v2 is connected successfully ✅\n\nPREFIX: ${prefix}`;
 
 conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://telegra.ph/file/900435c6d3157c98c3c88.jpg` }, caption: up })
 
