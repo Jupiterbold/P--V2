1439a65d63dc6bda081719c3fb250fea4e1eab62
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-02-18 22:18:57 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+    SESSION_ID: process.env.SESSION_ID || "your session id",
     ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/74NG4nf.jpeg",
-    ALIVE_MSG: process.env.ALIVE_MSG ||  "Hey there, I'm alive" 
+    ALIVE_MSG: process.env.ALIVE_MSG || "Hey there, I'm alive",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "your number",
+    MODE: process.env.MODE || "private",
+    PREFIX: process.env.PREFIX || ".",
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
 };
