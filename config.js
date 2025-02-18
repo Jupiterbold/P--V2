31abd3f904f0a22292c7485d387fbca6a6d9db80
Jupiterbold <158526130+Jupiterbold@users.noreply.github.com>
2025-02-18 22:46:01 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,12 +5,12 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "your session id",
+    SESSION_ID: process.env.SESSION_ID || "U5JQRKyB#A8S7kY546gzSV18r9MKN07Zzz-7DtgE698zhJUYE8lE",
     ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/74NG4nf.jpeg",
     ALIVE_MSG: process.env.ALIVE_MSG || "Hey there, I'm alive",
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "your number",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "2348084644182",
     MODE: process.env.MODE || "public",
     PREFIX: process.env.PREFIX || ".",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
-    OWNER_NAME: process.env.OWNER_NAME || "your name",
+    OWNER_NAME: process.env.OWNER_NAME || "eleven00¹",
 };
